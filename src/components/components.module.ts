453553c014aa28cb53cfc39ca203654461230cdf
andela-andrewmakenzi <andrew.makenzi@andela.com>
2017-09-22 16:40:15 +0100

    (feat): Add main components module

---
+++ src/components/components.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { ComponentsModuleComponent } from './components-module/components-module';
+
+@NgModule({
+	declarations: [ComponentsModuleComponent],
+	imports: [],
+	exports: [ComponentsModuleComponent]
+})
+export class ComponentsModule {}
